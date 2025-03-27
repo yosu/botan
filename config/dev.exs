4b95b0d1b3936fc70fe8d4c31bdb2f1344648b4e
yosu <woodstock830@gmail.com>
2025-03-27 09:08:06 +0900

    Change dev port

--- config/dev.exs
+++ config/dev.exs
@@ -19,7 +19,7 @@ config :botan, Botan.Repo,
 config :botan, BotanWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {127, 0, 0, 1}, port: 4020],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
