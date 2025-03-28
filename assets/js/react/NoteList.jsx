7db3b0b1edc9fa9dbe96979398cc1a222c54599e
yosu <woodstock830@gmail.com>
2025-03-28 14:22:06 +0900

    Add note list column

--- assets/js/react/NoteList.jsx
+++ assets/js/react/NoteList.jsx
@@ -1,11 +1,32 @@
-import { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 
 export const NoteList = ({ bookId }) => {
+  const [notes, setNotes] = useState([]);
 
   useEffect(() => {
-    // fetch("/api/notes").then(resp => {
-    //   console.log(resp)
-    // })
-    console.log("bookId", bookId)
+    (async () => {
+      const resp = await fetch(`/api/notes?book_id=${bookId}`);
+      if (!resp.ok) {
+        throw new Error(`レスポンスステータス: ${resp.status}`)
+      }
+
+      const json = await resp.json()
+      setNotes(json.data)
+    })()
   }, [bookId])
+
+  return (
+    <div>
+      {notes.map((note) => <Note key={note.id} note={note} />)}
+    </div>
+  );
+}
+
+
+const Note = ({ note }) => {
+  return (
+    <div className="border bottom-1 border-zinc-300">
+      <h3 className="select-none cursor-pointer hover:bg-orange-200">{note.title}</h3>
+    </div>
+  );
 }
