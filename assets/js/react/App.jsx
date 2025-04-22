d0308c11ab311d28c3ada8a624843a68dae6000c
yosu <woodstock830@gmail.com>
2025-04-22 09:58:39 +0900

    Add scrollbar to columns

--- assets/js/react/App.jsx
+++ assets/js/react/App.jsx
@@ -9,13 +9,13 @@ const App = ({ props }) => {
 
   return (
     <div id="wrapper" className="flex min-h-[calc(100vh-34px)]">
-      <div id="books" className="bg-red-100 w-56 p-2">
+      <div id="books" className="bg-red-100 p-2 w-56 h-screen overflow-y-auto">
         <BookTree books={props.books} />
       </div>
-      <div id="notes" className="bg-orange-100 w-56">
+      <div id="notes" className="bg-orange-100 w-56 h-screen overflow-y-auto">
         {bookId ? <NoteList bookId={bookId}/>: "No notebooks"}
       </div>
-      <div id="content" className="bg-yellow-50 w-full">
+      <div id="content" className="bg-yellow-50 w-full h-screen overflow-y-auto">
         {noteId ? <Note noteId={noteId} /> : "No contents"}
       </div>
     </div>
