5182fdb8393b692779abc169e87407d4b9d85377
yosu <woodstock830@gmail.com>
2025-04-25 16:37:54 +0900

    Add redux dependency

--- assets/js/react/Router.jsx
+++ assets/js/react/Router.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App";
+import store from "./store"
+import { Provider } from "react-redux"
 
 const Router = (props) => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="app">
-          <Route index element={<App props={props} />} />
-          <Route path=":bookId" element={<App props={props} />} />
-          <Route path=":bookId/:noteId" element={<App props={props} />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="app">
+            <Route index element={<App props={props} />} />
+            <Route path=":bookId" element={<App props={props} />} />
+            <Route path=":bookId/:noteId" element={<App props={props} />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   )
 }
 
