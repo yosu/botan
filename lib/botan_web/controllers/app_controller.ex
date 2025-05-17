17e29c5450b6589a531f17b85c1d692c396fc1dc
yosu <woodstock830@gmail.com>
2025-05-17 14:26:11 +0900

    Mix format

--- lib/botan_web/controllers/app_controller.ex
+++ lib/botan_web/controllers/app_controller.ex
@@ -4,7 +4,7 @@ defmodule BotanWeb.AppController do
   alias Botan.Editor
 
   def app(conn, _params) do
-    books = Editor.list_books() |> Enum.map(&(Map.take(&1, [:id, :name, :parent_book_id])))
+    books = Editor.list_books() |> Enum.map(&Map.take(&1, [:id, :name, :parent_book_id]))
     render(conn, :app, props: Jason.encode!(%{books: books}))
   end
 end
