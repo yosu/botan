3a198fc7196a1700ab9d69fa0b514ef95fd12d7f
yosu <woodstock830@gmail.com>
2025-05-20 15:21:14 +0900

    Resize if the importing image size is large

--- lib/botan/image.ex
+++ lib/botan/image.ex
@@ -2,7 +2,7 @@ defmodule Botan.Image do
   def compact!(name, data) do
     new_data =
       Image.from_binary!(data)
-      |> Image.thumbnail!(1200)
+      |> Image.thumbnail!(1200, resize: :down)
       |> Image.write!(:memory, suffix: ".webp")
 
     new_name = Path.rootname(name) <> ".webp"
